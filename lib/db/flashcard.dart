83a242748203648dd47574f6d5080446bd1a9b23
Nuel-Agyei <nuelagyei992@gmail.com>
2024-10-01 12:14:02 +0000

    db working

---
+++ lib/db/flashcard.dart
@@ -0,0 +1,11 @@
+import 'package:drift/drift.dart';
+
+class Flashcard extends Table {
+  TextColumn get id => text()();
+  TextColumn get deck => text()();
+  TextColumn get question => text()();
+  TextColumn get answer => text()();
+  TextColumn get image => text().nullable()();
+  TextColumn get audio => text().nullable()();
+  TextColumn get tag => text()();
+}
